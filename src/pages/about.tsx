db26184b0ac5507bee70e7338fdd56d3750dab32
AMW - Game & Entertainment <animemixedworld@gmail.com>
2021-02-28 18:27:47 +0100

    feat: Added pages required

---
+++ src/pages/about.tsx
@@ -0,0 +1,26 @@
+import { css, styled } from "twin.macro";
+import { Layout } from "@layouts/default";
+import { Header } from "@components/header";
+import { Translate } from "@components/translate"
+
+const Title = styled.h1(() => [
+    css`
+        height: 303px;
+        width: 587px;
+        color: #000000;
+        font-family: Larsseit;
+        font-size: 140px;
+        font-weight: bold;
+        letter-spacing: 0;
+        line-height: 160px;
+    `,
+]);
+
+export default function About() {
+    return (
+        <Layout>
+            <Header />
+            <Title><Translate id="about.title" /></Title>
+        </Layout>
+    );
+}
