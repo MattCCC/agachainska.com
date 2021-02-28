72e54b7bfe2f337d6572e7395ecfee79968b8261
MattCCC <deindesign.pl@gmail.com>
2021-02-28 21:35:11 +0100

    Apply waves

--- src/layouts/default.tsx
+++ src/layouts/default.tsx
@@ -1,6 +1,10 @@
 import { FunctionComponent } from "react";
-import { styled, css } from "twin.macro";
+import tw, { styled, css } from "twin.macro";
 import "@styles/layout.scss";
+import { ReactComponent as WavePattern1 } from "@svg/Path 2@1x.svg";
+import { ReactComponent as WavePattern2 } from "@svg/Path 2-1@1x.svg";
+import { ReactComponent as WavePattern3 } from "@svg/Path 2-2@1x.svg";
+import { ReactComponent as WavePattern4 } from "@svg/Path 2-3@1x.svg";
 
 /**
  * Styles
@@ -23,7 +27,8 @@ const Main = styled.main(({ hasGradient }: { hasGradient: boolean }) => [
         `,
 ]);
 
-const Bg = styled.div(() => [
+const Background = styled.div(() => [
+    tw`overflow-hidden`,
     css`
         position: absolute;
         top: 0;
@@ -34,6 +39,46 @@ const Bg = styled.div(() => [
         height: 100%;
         min-height: 100vh;
         width: 100%;
+    `,
+]);
+
+const Wave1 = styled(WavePattern1)(() => [
+    tw`absolute z-10`,
+    css`
+        width: 100vw;
+        background-size: contain;
+        height: 224.88px;
+        top: 59.5px;
+    `,
+]);
+
+const Wave2 = styled(WavePattern2)(() => [
+    tw`absolute z-10`,
+    css`
+        width: 100vw;
+        background-size: contain;
+        height: 219.12px;
+        top: 295.7px;
+    `,
+]);
+
+const Wave3 = styled(WavePattern3)(() => [
+    tw`absolute z-10`,
+    css`
+        width: 100vw;
+        background-size: contain;
+        height: 219.12px;
+        top: 435.7px;
+    `,
+]);
+
+const Wave4 = styled(WavePattern4)(() => [
+    tw`absolute z-10`,
+    css`
+        width: 100vw;
+        background-size: contain;
+        height: 219.12px;
+        top: 665.7px;
     `,
 ]);
 
@@ -49,7 +94,12 @@ interface Props {}
 export const Layout: FunctionComponent<Props> = ({ children }) => {
     return (
         <Main hasGradient>
-            <Bg />
+            <Background>
+                <Wave1 viewBox="0 0 1440 224.88" preserveAspectRatio="none" />
+                <Wave2 viewBox="0 0 1440 224.88" preserveAspectRatio="none" />
+                <Wave3 viewBox="0 0 1440 224.88" preserveAspectRatio="none" />
+                <Wave4 viewBox="0 0 1440 224.88" preserveAspectRatio="none" />
+            </Background>
             {children}
         </Main>
     );
