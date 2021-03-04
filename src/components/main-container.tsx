2374cc139d616101122793dc5c77c630b7892ee4
MattCCC <deindesign.pl@gmail.com>
2021-03-04 02:07:59 +0100

    feat: add main container component

---
+++ src/components/main-container.tsx
@@ -0,0 +1,19 @@
+import { FunctionComponent } from "react";
+
+/**
+ * Interfaces
+ */
+interface Props {
+    className?: string;
+}
+
+/**
+ * Component
+ * @param props
+ */
+export const MainContainer: FunctionComponent<Props> = ({
+    className,
+    children,
+}) => {
+    return <div className={`container mx-auto ${className}`}>{children}</div>;
+};
