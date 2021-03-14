3c184be871ed0c493722fbd1680d7943924d01bd
MattCCC <deindesign.pl@gmail.com>
2021-03-14 22:13:10 +0100

    chore: Improve store typings

--- src/utils/store.ts
+++ src/utils/store.ts
@@ -1,7 +1,9 @@
+type Funct<T, TT> = (_prevState: T, value: TT) => any;
+
 /**
  * Toggle state boolean
  * @param {string} type      State key
  */
-export const toggle = <T1, T2 = T1>(type: keyof T1): any => (_prevState: T2, bool: boolean): any => ({
-    [type]: bool,
+export const set = <T, T1, T2 = T>(type: keyof T): Funct<T2, T1> => (_prevState, value): any => ({
+    [type]: value || false,
 });
