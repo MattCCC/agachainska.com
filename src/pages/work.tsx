dff0551e274116408531c7f7007c554fcf1a463c
MattCCC <deindesign.pl@gmail.com>
2021-03-19 20:40:08 +0100

    feat: Remove work page animation

--- src/pages/work.tsx
+++ src/pages/work.tsx
@@ -5,19 +5,11 @@ import { MainContainer } from "@components/main-container";
 import { Header } from "@components/header";
 import { Timeline } from "@components/timeline";
 import { Slider } from "@components/slider";
-import { motion } from "@components/animation";
 import { workPageTimeline } from "@config/page-timlines";
 
-const transition = {
-    duration: 1,
-    ease: [0.43, 0.13, 0.23, 0.96],
-};
-
-const backVariants = {
-    exit: { y: 100, opacity: 0, transition },
-    enter: { y: 0, opacity: 1, transition: { delay: 0, ...transition } },
-};
-
+/**
+ * Styles
+ */
 const ContentContainer = styled.div(() => [
     tw`relative grid grid-cols-5 grid-rows-6 gap-y-6 grid-flow-col`,
     css`
@@ -39,37 +31,31 @@ const TimelineWrapper = styled.aside(() => [
  */
 export default function Work({ data }): JSX.Element {
     // eslint-disable-next-line @typescript-eslint/no-empty-function
-    const onTimelineItemChange = useCallback((): void => { }, []);
+    const onTimelineItemChange = useCallback((): void => {}, []);
 
     return (
         <Fragment>
-            <motion.div
-                className="single"
-                initial="exit"
-                animate="enter"
-                exit="exit"
-                variants={backVariants}
-            >
-                <Header />
-                <MainContainer className="lg:pt-20">
-                    <ContentContainer>
-                        <SlideWrapper>
-                            <Slider images={[
+            <Header />
+            <MainContainer className="lg:pt-20">
+                <ContentContainer>
+                    <SlideWrapper>
+                        <Slider
+                            images={[
                                 "./img/projects/image-1.png",
                                 "./img/projects/danish-bakery.jpg",
-                                "./img/projects/image-1.png"
-                            ]} />
-                        </SlideWrapper>
-                        <TimelineWrapper>
-                            <Timeline
-                                style={{ height: "27.76rem" }}
-                                onTimelineItemChange={onTimelineItemChange}
-                                sections={workPageTimeline}
-                            />
-                        </TimelineWrapper>
-                    </ContentContainer>
-                </MainContainer>
-            </motion.div>
+                                "./img/projects/image-1.png",
+                            ]}
+                        />
+                    </SlideWrapper>
+                    <TimelineWrapper>
+                        <Timeline
+                            style={{ height: "27.76rem" }}
+                            onTimelineItemChange={onTimelineItemChange}
+                            sections={workPageTimeline}
+                        />
+                    </TimelineWrapper>
+                </ContentContainer>
+            </MainContainer>
         </Fragment>
     );
 }
