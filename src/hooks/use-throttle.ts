4a92469b97c2f6f4752fe6853a570766440151ac
MattCCC <deindesign.pl@gmail.com>
2021-03-21 13:56:02 +0100

    chore: Add use throttle hook

---
+++ src/hooks/use-throttle.ts
@@ -0,0 +1,4 @@
+import { useCallback } from "react";
+import { throttle } from "lodash-es";
+
+export const useThrottle = (fnToThrottle: any, durationInMs: number = 200): any => useCallback(throttle(fnToThrottle, durationInMs), [fnToThrottle, durationInMs]);
