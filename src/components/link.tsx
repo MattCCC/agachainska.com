e9f91581c733616a6cb9fc3344dba08d9bd189cb
Redemer Pace <red.pace.dev@gmail.com>
2021-03-28 20:16:33 +0200

    Feat: Integrated delay link hook to link component

--- src/components/link.tsx
+++ src/components/link.tsx
@@ -3,14 +3,11 @@ import { FunctionComponent, ReactNode } from "react";
 import { Link as TranslatedLink } from "gatsby-plugin-intl";
 import { styled } from "twin.macro";
 
-import { fullPageOverlayDuration } from "@components/full-page-overlay";
 import {
     LinkDelayedArgs,
     LinkDelayedCallback,
-    useDelayLink,
 } from "@hooks/link-delayed";
-import { useLocation } from "@reach/router";
-import { useStore } from "@store/index";
+import { useDelayedLink } from "@hooks/use-delay-link";
 import { excludeProps } from "@utils/styled";
 
 /**
@@ -37,34 +34,17 @@ export const Link: FunctionComponent<Props> = ({
     to,
     replace = false,
     delay = 0,
-    onDelayStart = ((_e, _to) => {}) as LinkDelayedCallback,
-    onDelayEnd = ((_e, _to) => {}) as LinkDelayedCallback,
+    onDelayStart = ((_e, _to) => { }) as LinkDelayedCallback,
+    onDelayEnd = ((_e, _to) => { }) as LinkDelayedCallback,
     children,
     ...props
 }: Props) => {
-    const location = useLocation();
-    const [, dispatch] = useStore();
-
-    const startDelay = (e: Event, toRoute: string): void => {
-        dispatch.setCurrentDelayedRoute(toRoute);
-        dispatch.showMotionCursor(false);
-        onDelayStart(e, toRoute);
-    };
-
-    const endDelay = (e: Event, toRoute: string): void => {
-        dispatch.setCurrentDelayedRoute("");
-        dispatch.showMotionGrid(true);
-        dispatch.showWavePattern(true);
-        onDelayEnd(e, toRoute);
-    };
-
-    const onClick = useDelayLink({
-        location,
+    const { onClick } = useDelayedLink({
         to,
-        delay: fullPageOverlayDuration * 1000 * 1.1,
         replace,
-        onDelayStart: startDelay,
-        onDelayEnd: endDelay,
+        delay,
+        onDelayStart,
+        onDelayEnd,
     });
 
     return (
