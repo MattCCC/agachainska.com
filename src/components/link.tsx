90e6114eeab16697dd5e363cbef08aeb3c81cd93
MattCCC <deindesign.pl@gmail.com>
2021-04-01 18:32:12 +0200

    feat: Apply useNavigate to links

--- src/components/link.tsx
+++ src/components/link.tsx
@@ -3,8 +3,8 @@ import { FunctionComponent, ReactNode } from "react";
 import { Link as TranslatedLink } from "gatsby-plugin-intl";
 import { styled } from "twin.macro";
 
-import { useDelayedLink } from "@hooks/use-delay-link";
-import { LinkDelayedArgs, LinkDelayedCallback } from "@hooks/use-link-delayed";
+import { useNavigate } from "@hooks/use-delay-link";
+import { LinkDelayedArgs, OnDelayCallback } from "@hooks/use-link-delayed";
 import { excludeProps } from "@utils/styled";
 
 /**
@@ -30,12 +30,12 @@ export const Link: FunctionComponent<Props> = ({
     to,
     replace = false,
     delay = 0,
-    onDelayStart = ((_e, _to) => {}) as LinkDelayedCallback,
-    onDelayEnd = ((_e, _to) => {}) as LinkDelayedCallback,
+    onDelayStart = ((_e, _to) => {}) as OnDelayCallback,
+    onDelayEnd = ((_e, _to) => {}) as OnDelayCallback,
     children,
     ...props
 }: Props) => {
-    const { onClick } = useDelayedLink({
+    const onClick = useNavigate({
         to,
         replace,
         delay,
