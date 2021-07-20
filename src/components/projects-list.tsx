fa2b2077ee6bf384745e07c45282efab84aee6f3
Roudan Chirkoh <roudan.chirkoh@gmail.com>
2021-07-21 02:47:34 +0300

    feat: Projects list component

---
+++ src/components/projects-list.tsx
@@ -0,0 +1,41 @@
+import { Fragment, FunctionComponent } from "react";
+
+import tw, { styled, css } from "twin.macro";
+
+import { ProjectBadge } from "@domain/single-project/project-badge";
+
+
+const Table = styled.ol(() => [
+    tw`grid max-w-full grid-flow-row grid-cols-1 lg:grid-cols-2 lg:grid-flow-col gap-x-20 gap-y-8`,
+    css`
+        grid-template-rows: repeat(3, minmax(0, max-content));
+        width: 827px;
+        line-height: 24px;
+        margin-bottom: 80px;
+    `,
+]);
+
+interface Props {
+    nameSlug: string;
+    name: string;
+    category: string;
+    index: number;
+}
+
+export const SingleProject = ({nameSlug, name, category, index}: Props): JSX.Element => (
+    <Fragment>
+        <ProjectBadge
+            key={index}
+            index={index}
+            nameSlug={nameSlug}
+            name={name}
+            category={category}
+        />
+    </Fragment>
+);
+
+export const ProjectsList: FunctionComponent = ({children, ...props}) => (
+    <Table {...props}>
+        {children}
+    </Table>
+);
--- src/domain/single-project/other-projects.tsx
+++ src/domain/single-project/other-projects.tsx
@@ -1,8 +1,7 @@
 import { memo } from "react";
 
-import tw, { styled, css } from "twin.macro";
 
-import { ProjectBadge } from "@domain/single-project/project-badge";
+import { SingleProject, ProjectsList } from "@components/projects-list";
 import { ProjectsByCategory } from "@hooks/use-projects-by-category";
 
 interface Props {
@@ -10,31 +9,21 @@ interface Props {
     projectsByCategory: ProjectsByCategory;
 }
 
-const Table = styled.ol(() => [
-    tw`grid max-w-full grid-flow-row grid-cols-1 lg:grid-cols-2 lg:grid-flow-col gap-x-20 gap-y-8`,
-    css`
-        grid-template-rows: repeat(3, minmax(0, max-content));
-        width: 827px;
-        line-height: 24px;
-        margin-bottom: 80px;
-    `,
-]);
 
 export const OtherProjects = memo(
     ({ projectsByCategory, limit = 4, ...props }: Props): JSX.Element => (
-        <Table {...props}>
+        <ProjectsList {...props}>
             {projectsByCategory.others
                 .slice(0, limit)
                 .map(({ nameSlug, name, category }: Project, index: number) => (
-                    <ProjectBadge
-                        key={index}
-                        index={index}
+                    <SingleProject
                         nameSlug={nameSlug}
                         name={name}
                         category={category}
+                        index={index}
                     />
                 ))}
-        </Table>
+        </ProjectsList>
     ),
     (prev, next) =>
         prev.projectsByCategory.filteredProjects.length ===
