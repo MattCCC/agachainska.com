804bbb2b3e7c8cbc1ca9ff2ca5e62e6cb999a254
Roudan Chirkoh <roudan.chirkoh@gmail.com>
2021-07-30 17:12:28 +0300

    feat: Parallax background

--- src/components/about-parallax-background.tsx
+++ src/components/about-parallax-background.tsx
@@ -9,7 +9,6 @@ import tw, { css, styled } from "twin.macro";
 
 import { ReactComponent as GreekEyeIllustration } from "@svg/Greek eye@1x.svg";
 import { ReactComponent as LondonEyeIllustration } from "@svg/London eye@1x.svg";
-import { ReactComponent as CaipirinhaIllustration } from "@svg/London eye@1x.svg";
 import { ReactComponent as PricklyPearIllustration } from "@svg/Prickly pear@1x.svg";
 
 
@@ -31,9 +30,13 @@ const ParallaxCon = styled(motion.div)(() => [
 export const ParallaxBackground: FunctionComponent = () => {
 
     const { scrollY } = useViewportScroll();
-    const y1 = useTransform(scrollY, [0, 1000], [0, 100]);
+    const y1 = useTransform(scrollY, [0, 1100], [0, 200]);
     const y2 = useTransform(scrollY, [0, 2000], [0, -350]);
-    const y3 = useTransform(scrollY, [0, 300], [0, 400]);
+    const y3 = useTransform(scrollY, [0, 3000], [0, 100]);
+    const y4 = useTransform(scrollY, [0, 1800], [0, 500]);
+    const y5 = useTransform(scrollY, [0, 2200], [0, -250]);
+    const y6 = useTransform(scrollY, [0, 4500], [0, 200]);
+
 
     const Illustrations = [
         {
@@ -58,16 +61,16 @@ export const ParallaxBackground: FunctionComponent = () => {
             illustration: <GreekEyeIllustration />,
             top: "975px",
             left: "970px",
-            y: y2
+            y: y4
         },
         {
             illustration: <LondonEyeIllustration />,
             top: "1262px",
             left: "1158px",
-            y: y1
+            y: y2
         },
         {
-            illustration: <CaipirinhaIllustration />,
+            illustration: <LondonEyeIllustration />,
             top: "1392px",
             left: "512px",
             y: y3
@@ -84,13 +87,13 @@ export const ParallaxBackground: FunctionComponent = () => {
             illustration: <GreekEyeIllustration />,
             top: "2303px",
             left: "1116px",
-            y: y2
+            y: y6
         },
         {
             illustration: <LondonEyeIllustration />,
             top: "2073px",
             left: "1206px",
-            y: y1
+            y: y5
         },
     ];
 
