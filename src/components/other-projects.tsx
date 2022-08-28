ab1a93f0f5d49ffcd63e2e500338ae90ab68f40f
MattCCC <deindesign.pl@gmail.com>
2022-08-28 19:46:48 +0200

    fix: attempt to fix nth child err + optimize css selectors

--- src/components/other-projects.tsx
+++ src/components/other-projects.tsx
@@ -43,24 +43,12 @@ const OtherProjectsAnimVariants = {
 };
 
 const OtherProjectsContainer = styled(motion.div)(() => [
-    tw`grid grid-cols-2 col-start-1 col-end-5 row-start-1 auto-rows-[240px] -mt-16 duration-150 gap-x-28 gap-y-0`,
+    tw`grid grid-cols-2 col-start-1 col-end-5 row-start-1 -mt-16 duration-150 gap-x-28 gap-y-0`,
     css`
-        div[class*="other-project-container"]:nth-of-type(odd)::after {
-            content: "";
-            height: 5rem;
-        }
-
-        div[class*="other-project-container"]:nth-of-type(even) {
-            grid-row: span 2;
-            grid-column-start: 2;
+        & > div:nth-of-type(even) {
             position: relative;
-            padding-top: 8rem;
+            padding-top: 6rem;
             left: 30px;
-        }
-
-        div[class*="other-project-container"]:not(:nth-child(1)):nth-of-type(odd) {
-            position: relative;
-            top: 225px;
         }
 
         h1[class*="MainTitleTop"] {
