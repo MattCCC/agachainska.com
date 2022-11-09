4b6659805e103cffac3c371c181d378e299e91bd
Matt <deindesign.pl@gmail.com>
2022-11-09 23:08:23 +0100

    feat: Load iframes dynamically

--- src/components/device-mockup.tsx
+++ src/components/device-mockup.tsx
@@ -1,20 +1,33 @@
-import { memo, useEffect, useState, Fragment } from "react";
+import {
+    memo,
+    useEffect,
+    useState,
+    Fragment,
+    useRef,
+    MutableRefObject,
+} from "react";
 
+import { useInView } from "react-hook-inview";
 import tw, { css, styled } from "twin.macro";
+
+import isImageURL from "utils/is-image-url";
 
 import Iphone13Pro from "../img/iphone-13-pro.png";
 import Iphone8 from "../img/iphone-8.png";
 import IphoneX from "../img/iphone-x.png";
 import MacbookPro from "../img/macbook-pro.png";
 
-const MobileDeviceContainer = styled.div(() => [
-    tw`relative w-[245px] h-[560px]`,
-]);
+const DeviceContainer = styled.div(() => [tw`relative w-[245px] h-[560px]`]);
 
-const MobileDevice = styled.iframe(() => [
+const DeviceResourceWrapper = styled.div(() => [
     tw`absolute z-10 bg-white border-0 outline-none cursor-grabbing`,
     tw`w-[245px] h-[532px] rounded-[20px] top-[13px] left-[18px]`,
     tw`shadow-[0px_4px_12px_-1px_rgba(0,0,0,0.36)] lg:shadow-[0px_42px_102px_-8px_rgba(0,0,0,0.36)]`,
+]);
+
+const DeviceResource = styled.iframe(() => [
+    tw`bg-white border-0 outline-none`,
+    tw`w-[245px] h-[532px]`,
 ]);
 
 const DeviceFrameIphoneX = styled.div(() => [
@@ -55,51 +68,41 @@ const renderSwitch = ({
     type,
     link,
     isImage,
+    inViewData,
 }: {
     type: string;
     link: string;
     isImage: boolean;
+    inViewData: [(node: Element | null) => void, MutableRefObject<boolean>];
 }) => {
     const tag = isImage ? "img" : "iframe";
+    const [ref, lockRef] = inViewData;
 
-    switch (type) {
-        case "iPhoneX":
-            return (
-                <Fragment>
-                    <MobileDevice as={tag} src={link}></MobileDevice>
-                    <DeviceFrameIphoneX />
-                </Fragment>
-            );
+    return (
+        <Fragment>
+            <DeviceResourceWrapper ref={ref}>
+                {((lockRef.current && tag === "iframe") ||
+                    tag !== "iframe") && <DeviceResource as={tag} src={link} />}
+            </DeviceResourceWrapper>
 
-        case "iPhone13pro":
-            return (
-                <Fragment>
-                    <MobileDevice as={tag} src={link}></MobileDevice>
-                    <DeviceFrameIphone13Pro />
-                </Fragment>
-            );
-
-        case "iPhone8":
-            return (
-                <Fragment>
-                    <MobileDevice as={tag} src={link}></MobileDevice>
-                    <DeviceFrameIphone8 />
-                </Fragment>
-            );
-
-        case "laptop":
-        default:
-            return (
-                <Fragment>
-                    <MobileDevice as={tag} src={link}></MobileDevice>
-                    <DeviceFrameMacbookPro />
-                </Fragment>
-            );
-    }
+            {type === "iPhoneX" && <DeviceFrameIphoneX />}
+            {type === "iPhone13pro" && <DeviceFrameIphone13Pro />}
+            {type === "iPhone8" && <DeviceFrameIphone8 />}
+            {type === "laptop" && <DeviceFrameMacbookPro />}
+        </Fragment>
+    );
 };
 
 export const DeviceMockup = memo(({ type, link }: Props) => {
     const [isImage, setIsImage] = useState(false);
+    const [ref, isVisible] = useInView({
+        threshold: 1,
+    });
+    const lockRef = useRef(false);
+
+    if (isVisible) {
+        lockRef.current = true;
+    }
 
     useEffect(() => {
         const getData = () => {
@@ -112,8 +115,13 @@ export const DeviceMockup = memo(({ type, link }: Props) => {
     }, [link]);
 
     return (
-        <MobileDeviceContainer>
-            {renderSwitch({ isImage, type, link })}
-        </MobileDeviceContainer>
+        <DeviceContainer>
+            {renderSwitch({
+                isImage,
+                type,
+                link,
+                inViewData: [ref, lockRef],
+            })}
+        </DeviceContainer>
     );
 });
