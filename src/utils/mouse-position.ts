bd460daede94c688453fd9b46ed25344fc79c2c7
Matt <deindesign.pl@gmail.com>
2022-11-11 13:01:59 +0100

    fix: mouse position deprecation notice

--- src/utils/mouse-position.ts
+++ src/utils/mouse-position.ts
@@ -6,26 +6,21 @@ export default function getMousePosition(e: MouseEvent): {
     x: number;
     y: number;
 } {
+    if (typeof e.pageX !== "undefined") {
+        return { x: e.pageX, y: e.pageY };
+    }
+
     let posx = 0;
     let posy = 0;
 
-    if (!e) {
-        e = window.event as MouseEvent;
-    }
-
-    if (e.pageX || e.pageY) {
-        posx = e.pageX;
-        posy = e.pageY;
-    } else if (e.clientX || e.clientY) {
-        posx =
-            e.clientX +
-            document.body.scrollLeft +
-            document.documentElement.scrollLeft;
-        posy =
-            e.clientY +
-            document.body.scrollTop +
-            document.documentElement.scrollTop;
-    }
+    posx =
+        e.clientX +
+        document.body.scrollLeft +
+        document.documentElement.scrollLeft;
+    posy =
+        e.clientY +
+        document.body.scrollTop +
+        document.documentElement.scrollTop;
 
     return { x: posx, y: posy };
 }
