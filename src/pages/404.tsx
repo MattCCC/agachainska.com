944caeab650dfa5082101ea2d9cabe56ddc59489
Red <red.pace.dev@gmail.com>
2023-03-04 22:48:16 +0100

    feat: Added translations to 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,4 +1,6 @@
-import { NextPage } from "next";
+import { GetStaticProps, NextPage } from "next";
+
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 
 import { ErrorContainer } from "components/404";
 import { Meta } from "components/meta";
@@ -6,5 +8,11 @@ import { Meta } from "components/meta";
 const ErrorPage: NextPage = () => <ErrorContainer />;
 
 export default ErrorPage;
+
+export const getStaticProps: GetStaticProps = async ({ locale = "en" }) => ({
+  props: {
+    ...(await serverSideTranslations(locale)),
+  },
+});
 
 export const Head = () => <Meta title="404 - Aga Chainska" />;
