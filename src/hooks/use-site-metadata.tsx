cac1ed816a89570130b3c82722016d9a262f8bb1
Red <red.pace.dev@gmail.com>
2023-04-04 01:07:54 +0200

    fix: Removed graphql fragments

--- src/hooks/use-site-metadata.tsx
+++ src/hooks/use-site-metadata.tsx
@@ -1,23 +1,7 @@
-export const useSiteMetadata = () => {
-    // const data = useStaticQuery(graphql`
-    //     query {
-    //         site {
-    //             siteMetadata {
-    //                 title
-    //                 description
-    //                 twitterUsername
-    //                 image
-    //                 siteUrl
-    //             }
-    //         }
-    //     }
-    // `);
-
-    return {
-        title: '',
-        description: '',
-        image: '',
-        siteUrl: '',
-        twitterUsername: '',
-    };
-};
+export const useSiteMetadata = () => ({
+    title: "",
+    description: "",
+    image: "",
+    siteUrl: "",
+    twitterUsername: "",
+});
