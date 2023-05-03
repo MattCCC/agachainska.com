e1fa55c77f919d07a7d7f8639458400b5c6d3946
Matt <deindesign.pl@gmail.com>
2023-05-03 11:56:20 +0200

    feat: Limit selected projects

--- src/components/selected-projects.tsx
+++ src/components/selected-projects.tsx
@@ -4,23 +4,29 @@ import { Project } from "types/project";
 
 interface Props {
     projects: Project[];
+    limit: number;
 }
 
-export default function SelectedProjects({ projects }: Props) {
+export default function SelectedProjects({ projects, limit = 4 }: Props) {
     return (
         <ProjectsList>
-            {projects.map(
-                ({ _sys, name, category, cover }: Project, index: number) => (
-                    <SingleProject
-                        key={name}
-                        nameSlug={_sys.filename}
-                        name={name}
-                        category={category}
-                        index={index}
-                        cover={cover}
-                    />
-                )
-            )}
+            {projects
+                .slice(0, limit)
+                .map(
+                    (
+                        { _sys, name, category, cover }: Project,
+                        index: number
+                    ) => (
+                        <SingleProject
+                            key={name}
+                            nameSlug={_sys.filename}
+                            name={name}
+                            category={category}
+                            index={index}
+                            cover={cover}
+                        />
+                    )
+                )}
             <SeeAllProjectsLink screenSize="sm" />
         </ProjectsList>
     );
