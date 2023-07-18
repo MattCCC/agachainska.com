50e369c62551a9a27f0d6848c7344fa0d1652626
Roudan Chirkoh <roudan.chirkoh@gmail.com>
2023-07-18 23:30:17 +0300

    refactor: Use TW for see all projects button

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -6,23 +6,10 @@ interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {}
 
 const CustomButton = styled.button(() => [
     tw`relative transition-[top,left] ease-in left-0 top-0 select-none bg-tertiary`,
-    tw`lg:hover:top-[3px] lg:hover:left-[3px] duration-100`,
+    tw`lg:hover:top-[3px] lg:hover:left-[3px] lg:hover:text-tertiary duration-100`,
     css`
-        span {
-            ${tw`relative z-10 inline-block w-full h-full border border-white border-solid bg-tertiary`}
-
-            padding-left: 25px;
-            padding-right: 25px;
-            padding-top: 5px;
-            padding-bottom: 5px;
-        }
-
         @media screen and (min-width: 1024px) {
             &:hover {
-                span {
-                    ${tw`transition-all bg-primary text-tertiary`}
-                }
-
                 div {
                     top: 0px;
                     left: 0px;
@@ -32,13 +19,19 @@ const CustomButton = styled.button(() => [
     `,
 ]);
 
+const CustomButtonText = styled.span(() => [
+    tw`relative z-10 inline-block w-full h-full border border-white border-solid bg-tertiary`,
+    tw`px-[25px] py-[5px] lg:hover:transition-all lg:hover:bg-primary lg:hover:text-tertiary`,
+]);
+
 const Background = styled.div(() => [
     tw`absolute w-full h-full bg-[url("/img/button-bg.png")] bg-no-repeat bg-cover top-[6px] left-[6px] z-0 border border-black duration-100`,
+    tw`lg:hover:top-0 lg:hover:left-0`,
 ]);
 
 export const Button = ({ children, ...props }: PropsWithChildren<Props>) => (
     <CustomButton {...props}>
         <Background />
-        <span>{children}</span>
+        <CustomButtonText>{children}</CustomButtonText>
     </CustomButton>
 );
