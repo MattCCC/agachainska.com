dc262e655f3053acc3605e7a223c9b79e0197e01
Matt <deindesign.pl@gmail.com>
2023-12-13 22:23:47 +0100

    perf: Optimize timeline loading

--- src/components/timeline.tsx
+++ src/components/timeline.tsx
@@ -12,13 +12,12 @@ import {
 
 import tw, { css, styled } from "twin.macro";
 
-import { motion, MotionProps, AnimatePresence } from "framer-motion";
 import { useElementSize } from "hooks/use-element-size";
 import { excludeProps } from "utils/styled";
 
 const TimelineWrapper = styled.div(() => [tw`relative text-right z-100`]);
 
-const Title = styled(motion.div).withConfig(
+const Title = styled.div.withConfig(
     excludeProps(["hasMultipleSections", "isActive"])
 )(({ isActive, hasMultipleSections }: TitleStyle) => [
     tw`font-bold select-none lg:text-[20px] lg:leading-7 lg:text-primary opacity-30`,
@@ -31,11 +30,13 @@ const Title = styled(motion.div).withConfig(
     hasMultipleSections && tw`cursor-pointer`,
 ]);
 
-const List = styled(motion.div)(() => [
-    tw`relative flex flex-col items-end w-auto overflow-hidden font-bold lg:text-[14px] lg:leading-2 lg:text-gray-500 justify-evenly`,
+const TimelineList = styled.div(() => [
+    tw`relative flex flex-col items-end w-auto overflow-hidden font-bold lg:text-[14px] lg:leading-2 lg:text-gray-500 justify-evenly opacity-0 h-0`,
     css`
         box-shadow: inset -2px 0 0 -1px rgba(0, 0, 0, 0.2);
-        transition: opacity 0.3s ease-in-out;
+        transition:
+            opacity 0.3s cubic-bezier(0.04, 0.62, 0.23, 0.98),
+            height 0.3s cubic-bezier(0.04, 0.62, 0.23, 0.98);
     `,
 ]);
 
@@ -49,7 +50,7 @@ const Pin = styled.div(() => [
     tw`absolute top-0 bottom-0 right-0 w-px bg-primary z-[2] transition-transform transform-gpu`,
 ]);
 
-interface TitleStyle extends MotionProps {
+interface TitleStyle {
     isActive?: boolean;
     hasMultipleSections?: boolean;
 }
@@ -181,89 +182,75 @@ const Timeline = memo(
 
         return (
             <TimelineWrapper ref={wrapperRef} style={style}>
-                <AnimatePresence initial={false}>
-                    {sections.map((section: TimelineSection) => (
-                        <div key={`timeline-${section.id}`}>
-                            <Title
-                                isActive={section.id === state.activeSectionId}
-                                hasMultipleSections={sections.length > 1}
-                                initial="false"
-                                ref={sectionTitleRef}
-                                onClick={onTimelineHeaderClick.bind(
-                                    null,
-                                    section
-                                )}
-                                key={`timeline-${section.id}-title`}
-                            >
-                                {section.title}
-                            </Title>
-                            <List
-                                animate="open"
-                                initial="collapsed"
-                                exit="collapsed"
-                                key={`timeline-${section.id}-list`}
-                                variants={{
-                                    open: {
-                                        opacity: 1,
-                                        height:
-                                            section.id === state.activeSectionId
-                                                ? contentListHeight
-                                                : 0,
-                                    },
-                                    collapsed: { opacity: 0, height: 0 },
-                                }}
-                                transition={{
-                                    duration: 0.8,
-                                    ease: [0.04, 0.62, 0.23, 0.98],
-                                }}
-                            >
-                                <Pin
-                                    style={
-                                        {
-                                            "--tw-translate-y":
-                                                Math.max(
-                                                    0,
-                                                    (contentListHeight /
-                                                        (section.items
-                                                            ?.length ?? 1)) *
-                                                        (section.items?.findIndex(
-                                                            (item) =>
-                                                                item.id ===
-                                                                state.activeItemId
-                                                        ) ?? 0)
-                                                ) + "px",
-                                            height: Math.max(
+                {sections.map((section: TimelineSection) => (
+                    <div key={`timeline-${section.id}`}>
+                        <Title
+                            isActive={section.id === state.activeSectionId}
+                            hasMultipleSections={sections.length > 1}
+                            ref={sectionTitleRef}
+                            onClick={onTimelineHeaderClick.bind(null, section)}
+                            key={`timeline-${section.id}-title`}
+                        >
+                            {section.title}
+                        </Title>
+                        <TimelineList
+                            key={`timeline-${section.id}-list`}
+                            style={{
+                                opacity:
+                                    section.id === state.activeSectionId
+                                        ? 1
+                                        : 0,
+                                height:
+                                    section.id === state.activeSectionId
+                                        ? contentListHeight
+                                        : 0,
+                            }}
+                        >
+                            <Pin
+                                style={
+                                    {
+                                        "--tw-translate-y":
+                                            Math.max(
                                                 0,
-                                                contentListHeight /
-                                                    (section.items?.length ?? 1)
-                                            ),
-                                        } as React.CSSProperties
-                                    }
-                                />
+                                                (contentListHeight /
+                                                    (section.items?.length ??
+                                                        1)) *
+                                                    (section.items?.findIndex(
+                                                        (item) =>
+                                                            item.id ===
+                                                            state.activeItemId
+                                                    ) ?? 0)
+                                            ) + "px",
+                                        height: Math.max(
+                                            0,
+                                            contentListHeight /
+                                                (section.items?.length ?? 1)
+                                        ),
+                                    } as React.CSSProperties
+                                }
+                            />
 
-                                {section.items?.map((item: TimelineItem) => (
-                                    <ListItem
-                                        key={`${section.id}-${item.id}`}
-                                        className={
-                                            "opacity-30" +
-                                            (section.id ===
-                                                state.activeSectionId &&
-                                            item.id === state.activeItemId
-                                                ? "opacity-100"
-                                                : "")
-                                        }
-                                        onClick={onTimelineItemClick.bind(
-                                            null,
-                                            item
-                                        )}
-                                    >
-                                        {item.title}
-                                    </ListItem>
-                                ))}
-                            </List>
-                        </div>
-                    ))}
-                </AnimatePresence>
+                            {section.items?.map((item: TimelineItem) => (
+                                <ListItem
+                                    key={`${section.id}-${item.id}`}
+                                    className={
+                                        "opacity-30" +
+                                        (section.id === state.activeSectionId &&
+                                        item.id === state.activeItemId
+                                            ? "opacity-100"
+                                            : "")
+                                    }
+                                    onClick={onTimelineItemClick.bind(
+                                        null,
+                                        item
+                                    )}
+                                >
+                                    {item.title}
+                                </ListItem>
+                            ))}
+                        </TimelineList>
+                    </div>
+                ))}
             </TimelineWrapper>
         );
     },
