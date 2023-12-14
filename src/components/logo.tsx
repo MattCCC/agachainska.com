1f49bc3d26d31e614383a52985f1fe4a68da3487
Matt <deindesign.pl@gmail.com>
2023-12-14 03:27:37 +0100

    perf: Logo

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,4 +1,3 @@
-import tw, { css, styled } from "twin.macro";
 import { useStoreProp } from "store/index";
 
 import { useRouter } from "next/router";
@@ -7,40 +6,31 @@ import { Link } from "components/link";
 import { Translate } from "components/translate";
 import { getLinkProps } from "utils/route";
 
-const LogoWrapper = styled.div(() => [tw`flex items-center h-12 text-primary`]);
-
-const SiteTitle = styled.div(() => [
-    tw`subpixel-antialiased select-none font-flight text-[24px] text-primary leading-[30px]`,
-]);
-
-const LogoIcon = styled.div(() => [
-    tw`block text-center rounded-full select-none font-fbold text-tertiary bg-primary text-[18px] lg:leading-5 lg:hidden`,
-    css`
-        width: 48px;
-        height: 48px;
-        line-height: 48px;
-    `,
-]);
-
 export function Logo() {
     const [showLogoOnDesktop] = useStoreProp("showLogoOnDesktop");
     const location = useRouter();
     const homeLink = getLinkProps("home", location);
 
     return (
-        <LogoWrapper>
-            <SiteTitle
-                className={showLogoOnDesktop ? "hidden lg:block" : "hidden"}
+        <div className="flex items-center h-12 text-primary">
+            <div
+                className={`subpixel-antialiased select-none font-flight text-[24px] text-primary leading-[30px] hidden${
+                    showLogoOnDesktop ? " lg:block" : ""
+                }`}
             >
                 <Link {...homeLink}>
                     <Translate id="header.title" />
                 </Link>
-            </SiteTitle>
-            <LogoIcon>
+            </div>
+            <div
+                className={
+                    "block text-center rounded-full select-none font-fbold text-tertiary bg-primary text-[18px] lg:leading-5 lg:hidden w-[48px] h-[48px] leading-[48px]"
+                }
+            >
                 <Link {...homeLink}>
                     <Translate id="header.logo.title" />
                 </Link>
-            </LogoIcon>
-        </LogoWrapper>
+            </div>
+        </div>
     );
 }
